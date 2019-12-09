da2f28e7bde48aa406b005070a0cc2caf313af58
Alexander <vorobyovvorobyov@gmail.com>
2019-12-09 23:14:54 +0200

    Update application

--- Packages/UGF.Module.Coroutines/Runtime/CoroutineModuleDescription.cs
+++ Packages/UGF.Module.Coroutines/Runtime/CoroutineModuleDescription.cs
@@ -1,13 +1,7 @@
-using System;
-using UnityEngine;
-
 namespace UGF.Module.Coroutines.Runtime
 {
-    [Serializable]
     public class CoroutineModuleDescription : ICoroutineModuleDescription
     {
-        [SerializeField] private bool m_dontDestroyOnLoadExecuter = true;
-
-        public bool DontDestroyOnLoadExecuter { get { return m_dontDestroyOnLoadExecuter; } set { m_dontDestroyOnLoadExecuter = value; } }
+        public bool DontDestroyOnLoadExecuter { get; set; } = true;
     }
 }
