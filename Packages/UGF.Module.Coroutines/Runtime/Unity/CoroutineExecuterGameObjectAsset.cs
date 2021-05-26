eefe817a52a036b7ce2cea45818fa14fcb084077
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-05-27 01:14:30 +0300

    Change module to work with updated executers

---
+++ Packages/UGF.Module.Coroutines/Runtime/Unity/CoroutineExecuterGameObjectAsset.cs
@@ -0,0 +1,19 @@
+﻿using UGF.Coroutines.Runtime;
+using UGF.Coroutines.Runtime.Unity;
+using UnityEngine;
+
+namespace UGF.Module.Coroutines.Runtime.Unity
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Coroutines/Coroutine Executer GameObject", order = 2000)]
+    public class CoroutineExecuterGameObjectAsset : CoroutineExecuterAsset
+    {
+        [SerializeField] private string m_name = "CoroutineExecuterUnity";
+
+        public string Name { get { return m_name; } set { m_name = value; } }
+
+        protected override ICoroutineExecuter OnBuild()
+        {
+            return new CoroutineExecuterGameObject(m_name);
+        }
+    }
+}
