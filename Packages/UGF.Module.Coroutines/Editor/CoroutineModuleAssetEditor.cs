c733421d1de13afb19599c1f07a69c422ec1e0fc
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2022-01-06 15:38:27 +0300

    Update CoroutineModuleAssetEditor.cs

--- Packages/UGF.Module.Coroutines/Editor/CoroutineModuleAssetEditor.cs
+++ Packages/UGF.Module.Coroutines/Editor/CoroutineModuleAssetEditor.cs
@@ -1,4 +1,5 @@
-﻿using UGF.EditorTools.Editor.IMGUI.AssetReferences;
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.AssetReferences;
 using UGF.EditorTools.Editor.IMGUI.Scopes;
 using UGF.Module.Coroutines.Runtime;
 using UnityEditor;
@@ -8,21 +9,30 @@ namespace UGF.Module.Coroutines.Editor
     [CustomEditor(typeof(CoroutineModuleAsset), true)]
     internal class CoroutineModuleAssetEditor : UnityEditor.Editor
     {
-        private SerializedProperty m_propertyScript;
         private SerializedProperty m_propertyDefaultExecuter;
         private AssetReferenceListDrawer m_listExecuters;
+        private ReorderableListSelectionDrawerByPath m_listExecutersSelection;
 
         private void OnEnable()
         {
-            m_propertyScript = serializedObject.FindProperty("m_Script");
             m_propertyDefaultExecuter = serializedObject.FindProperty("m_defaultExecuter");
             m_listExecuters = new AssetReferenceListDrawer(serializedObject.FindProperty("m_executers"));
 
+            m_listExecutersSelection = new ReorderableListSelectionDrawerByPath(m_listExecuters, "m_asset")
+            {
+                Drawer =
+                {
+                    DisplayTitlebar = true
+                }
+            };
+
             m_listExecuters.Enable();
+            m_listExecutersSelection.Enable();
         }
 
         private void OnDisable()
         {
+            m_listExecutersSelection.Disable();
             m_listExecuters.Disable();
         }
 
@@ -30,14 +40,12 @@ namespace UGF.Module.Coroutines.Editor
         {
             using (new SerializedObjectUpdateScope(serializedObject))
             {
-                using (new EditorGUI.DisabledScope(true))
-                {
-                    EditorGUILayout.PropertyField(m_propertyScript);
-                }
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
 
                 EditorGUILayout.PropertyField(m_propertyDefaultExecuter);
 
                 m_listExecuters.DrawGUILayout();
+                m_listExecutersSelection.DrawGUILayout();
             }
         }
     }
